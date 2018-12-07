1d0fa447568f52fc0d97fdf224e0d64de76a7121
bambuseribu <44926213+bambuseribu@users.noreply.github.com>
2018-12-07 19:30:05 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh4jQu6p9sLSTftuxo4JtfWS6l4PMlJu_m5ichIqw0ICKj5SurYFqtMt6la2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "6BEE144CD23F4FB9";
+$os="android";
+$sign="abb583076119190458a319ba42ada5e591e59df07ac688ca2bd8b1deacf2c16d";
